dcef116532dc11d4506bc0d3580ddd15113e81ea
Ole-Christoffer Granmo <ole.granmo@uia.no>
2019-12-05 23:44:18 +0100

    Add files via upload

---
+++ examples/2DNoisyXORDemo.py
@@ -0,0 +1,27 @@
+#!/usr/local/bin/python3
+
+from pyTsetlinMachineMT.tm import MultiClassConvolutionalTsetlinMachine2D
+import numpy as np 
+
+train_data = np.loadtxt("2DNoisyXORTrainingData.txt")
+X_train = train_data[:,0:-1].reshape(train_data.shape[0], 4, 4)
+Y_train = train_data[:,-1]
+
+test_data = np.loadtxt("2DNoisyXORTestData.txt")
+X_test = test_data[:,0:-1].reshape(test_data.shape[0], 4, 4)
+Y_test = test_data[:,-1]
+
+ctm = MultiClassConvolutionalTsetlinMachine2D(40, 60, 3.9, (2, 2), boost_true_positive_feedback=0)
+
+ctm.fit(X_train, Y_train, epochs=5000)
+
+print("Accuracy:", 100*(ctm.predict(X_test) == Y_test).mean())
+
+Xi = np.array([[[0,1,1,0],
+		[1,1,0,1],
+		[1,0,1,1],
+		[0,0,0,1]]])
+
+print("\nInput Image:\n")
+print(Xi)
+print("\nPrediction: %d" % (ctm.predict(Xi)))

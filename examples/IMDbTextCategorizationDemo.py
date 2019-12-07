d88d919c824555023b5e4addf4d0d22cc111cbaf
Ole-Christoffer Granmo <ole.granmo@uia.no>
2019-12-07 22:42:15 +0100

    Update IMDbTextCategorizationDemo.py

--- examples/IMDbTextCategorizationDemo.py
+++ examples/IMDbTextCategorizationDemo.py
@@ -108,7 +108,7 @@ selected_features = SKB.get_support(indices=True)
 X_train = SKB.transform(X_train)
 X_test = SKB.transform(X_test)
 
-tm = MultiClassTsetlinMachine(10000, 80, 27.0)
+tm = MultiClassTsetlinMachine(10000, 80*100, 10.0, weighted_clauses=True)
 
 print("\nAccuracy over 50 epochs:\n")
 for i in range(50):

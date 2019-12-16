391bf5a212e70b1202ef84552a797a1279bcc015
Ole-Christoffer Granmo <ole.granmo@uia.no>
2019-12-16 21:35:25 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
    url='https://github.com/cair/pyTsetlinMachineParallel/',
    license='MIT',
    description='Multi-threaded implementation of the Tsetlin Machine, Convolutional Tsetlin Machine, Regression Tsetlin Machine, and Weighted Tsetlin Machine, with support for continuous features and multigranularity.',
-   long_description='Multi-threaded implementation the Tsetlin Machine, Convolutional Tsetlin Machine, Regression Tsetlin Machine, and Weighted Tsetlin Machine, with support for continuous features and multigranularity.',
+   long_description='Multi-threaded implementation of the Tsetlin Machine, Convolutional Tsetlin Machine, Regression Tsetlin Machine, and Weighted Tsetlin Machine, with support for continuous features and multigranularity.',
    ext_modules = [libTM],
    keywords ='pattern-recognition machine-learning interpretable-machine-learning rule-based-machine-learning propositional-logic tsetlin-machine regression convolution',
    packages=['pyTsetlinMachineParallel']
